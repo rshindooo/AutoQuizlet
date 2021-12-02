582df75438a3196438f3f2e3abd9d58a8d07111c
rshindooo <66354051+rshindooo@users.noreply.github.com>
2021-12-02 14:52:57 -0800

    Update proto.py

--- proto.py
+++ proto.py
@@ -1,21 +1,30 @@
+
 import time
 import pynput.mouse
 import keyboard
 from pynput.mouse import Button, Controller
 import sys
-def exit():
-    while True:
+e_num=-1
+def mains():
+    #while True:
         if keyboard.is_pressed("x"):
             print('STOP THE CODE')
             sys.exit(0)
+        if keyboard.is_pressed("s"):
+            pass
+            #run def
+mains()
 mouse= Controller()
 mouse.position=(0,10)
 
-timex=10
-while timex>0:
-    exit()
-    time.sleep(1)
-    timex-=1
+def email_gen(og):
+    global e_num +=1
+    keyboard.write("cum")
+#    keyboard.write(og.split('@',1)[0], "+",e_num,"og.split('@',1)[1])
+    return 0
 """
+x      x
 
 xxxxxxxx"""
+
+email_gen("test@gmail.com")
