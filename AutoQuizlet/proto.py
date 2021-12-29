92195a8ba0a08c7be50044c40752dc23d9f8cefa
rshindooo <66354051+rshindooo@users.noreply.github.com>
2021-12-29 14:12:33 -0800

    Update proto.py

--- AutoQuizlet/proto.py
+++ AutoQuizlet/proto.py
@@ -1,9 +1,9 @@
 import time
-#import pynput.mouse
 import keyboard
 import pyautogui
-#from pynput.mouse import Button, Controller
 import sys
+import pathlib
+import os
 def mains():
     #while True:
         if keyboard.is_pressed("x"):
@@ -12,23 +12,36 @@ def mains():
         if keyboard.is_pressed("s"):
             autoSign()
 mains()
-#mouse= Controller()
-#mouse.position=(0,10)
 e_num=0
-currentPath="C:/Python Stuff/{}"  
+print(os.getcwd()+"/{}")
+currentPath=os.getcwd()+"/{}"
 def email_gen(og):
     global e_num
     e_num +=1
     testi="{beforeat}+{e_n}@{afterat}".format(beforeat=og.split('@',1)[0],
                                             e_n=e_num, afterat=og.split('@',1)[1])
-    print(testi)
-    return(0)
-email_gen("test@gmail.com")
+    return(testi)
 email_gen("test@gmail.com")
 
-def autoSign():
-    #pyautogui.click(x.format("signup.png"))
-    #time.sleep(1)
-    pyautogui.click(x.format("day.png"))
+def autoSign(sampleE):
+    pyautogui.move(50,0)
+    for i in ("signup1.png","month.png","monthS.png", "day.png","dayS.png","year.png","yearS.png","email.png","password.png","signup.png"):
+        try:
+            x,y=pyautogui.locateCenterOnScreen(currentPath.format(i))
+        except TypeError:
+            print("where")
+        else:
+            pyautogui.click(currentPath.format(i))
+            print(i)
+            time.sleep(.05)
+            if(i=="signup1.png"):
+                time.sleep(.4)
+            if(i=="email.png"):
+                keyboard.write(email_gen(sampleE))
+                time.sleep(.4)
+            if(i=="password.png"):
+                keyboard.write("12345678a0")
+                time.sleep(1)
 
-autoSign()
+autoSign("cumballs@gmail.com")
+
