6927aad63d3e38472ffb0f93b7e65ba2cf8af83e
rshindooo <66354051+rshindooo@users.noreply.github.com>
2022-01-01 22:01:58 -0800

    Final Ver

--- AutoQuizlet/proto.py
+++ AutoQuizlet/proto.py
@@ -2,17 +2,12 @@ import time
 import keyboard
 import pyautogui
 import sys
+import random
+import string
 import pathlib
 import os
-def mains():
-    #while True:
-        if keyboard.is_pressed("x"):
-            print('STOP THE CODE')
-            sys.exit(0)
-        if keyboard.is_pressed("s"):
-            autoSign()
-mains()
 e_num=0
+isToggled = False
 print(os.getcwd()+"/{}")
 currentPath=os.getcwd()+"/{}"
 def email_gen(og):
@@ -24,24 +19,40 @@ def email_gen(og):
 email_gen("test@gmail.com")
 
 def autoSign(sampleE):
-    pyautogui.move(50,0)
-    for i in ("signup1.png","month.png","monthS.png", "day.png","dayS.png","year.png","yearS.png","email.png","password.png","signup.png"):
-        try:
-            x,y=pyautogui.locateCenterOnScreen(currentPath.format(i))
-        except TypeError:
-            print("where")
-        else:
-            pyautogui.click(currentPath.format(i))
-            print(i)
-            time.sleep(.05)
-            if(i=="signup1.png"):
-                time.sleep(.4)
-            if(i=="email.png"):
-                keyboard.write(email_gen(sampleE))
-                time.sleep(.4)
-            if(i=="password.png"):
-                keyboard.write("12345678a0")
-                time.sleep(1)
+        pyautogui.move(50,0)
+        for i in ("signup1.png","month.png","monthS.png", "day.png","dayS.png","year.png","yearS.png","email.png","password.png","face.png","signup.png"):
+            try:
+                x,y=pyautogui.locateCenterOnScreen(currentPath.format(i))
+            except TypeError:
+                print("where")
+            else:
+                
+                time.sleep(.17)
+                pyautogui.click(currentPath.format(i))
+                print(i)
+                if(i=="signup1.png"):
+                    time.sleep(2)
+                if(i=="email.png"):
+                    keyboard.write(email_gen(sampleE))
+                    time.sleep(.4)
+                if(i=="password.png"):
+                    keyboard.write("asfa2!!!3*6")
+                    time.sleep(1)
+                    keyboard.write("u")
+                    time.sleep(.5)
 
-autoSign("cumballs@gmail.com")
 
+def mains():
+    letters = string.ascii_lowercase
+    rGen= ( ''.join(random.choice(letters) for i in range(10)) )
+    while True:
+        if keyboard.is_pressed("x"):
+            print('STOP THE CODE')
+            sys.exit(0)
+        if keyboard.is_pressed("s"):
+            autoSign(rGen+"@gmail.com")
+
+
+
+print("TO USE THIS BOT RUN IT, THEN PRESS S TO MAKE 1 ACCOUNT. PRESS X TO EXIT AFTER BOT HAS FINISHED MAKING CODE. TO DISABLE BOTMAKING PRESS Q")
+mains()
